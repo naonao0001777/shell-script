398423af5e986a325591073a47ed2a94b109be7f
nao <46675984+naonao0001777@users.noreply.github.com>
2021-02-12 03:11:42 +0900

    Update remove-current-directory.sh

--- remove-current-directory.sh
+++ remove-current-directory.sh
@@ -1,2 +1,6 @@
-ls > a.txt
-while read a;do rm ./$a;done < /c/temp/a.txt
+# date
+DATE=`date +%Y%m%d%H%M%S`
+# list
+ls > /c/temp/${DATE}.txt
+# roop
+while read a;do rm ./$a;done < /c/temp/${DATE}.txt
